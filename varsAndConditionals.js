70a5259acc8fd24aaa688cfabc965f57767f4bbd
“Ilyass <ilyass.has@live.fr>
2023-09-13 15:06:18 -0400

    adds jamie attack

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -10,7 +10,18 @@ let jamieLannisterAttack = 35
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log('Jon Snow has better attack than Jamie Lannister')
 } else if (jamieLannisterAttack > jonSnowAttack) {
-console.log('Jamie Lannister has better attack than Jon Snow')
+    console.log('Jamie Lannister has better attack than Jon Snow')
 } else {
     console.log('Our fighters are evenly matched')
+}
+
+let jonSnowHealth  = 100
+let jonSnowDefense = 0
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log('Jon Snow been slain')
+} else {
+    jonSnowHealth = jonSnowHealth - jamieLannisterAttack
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`)
+    
 }
